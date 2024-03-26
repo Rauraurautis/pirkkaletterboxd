8b6a5204a8b66d7adacc4fa7705ed4eb7d03b8bb
Rauraurautis <rautanokka@gmail.com>
2024-03-26 12:49:49 +0200

    test changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import './assets/styles/index.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
